52b5ed29383b79aef66d795e11e6e8e27bae5c39
Fabrice Desré <fabrice@desre.org>
2016-02-03 00:48:14 -0800

    check service availability

--- src/websocket_server.rs
+++ src/websocket_server.rs
@@ -4,7 +4,7 @@
 
 use context::SharedContext;
 use std::thread;
-use ws::{listen, Handler, Sender, Result, Message, CloseCode};
+use ws::{Builder, Error, ErrorKind, Factory, Handler, Handshake, Sender, Request, Response, Result, Message, CloseCode};
 
 pub struct WebsocketServer {
     context: SharedContext
@@ -12,9 +12,38 @@ pub struct WebsocketServer {
 
 struct WebsocketHandler {
     out: Sender,
+    context: SharedContext
 }
 
 impl Handler for WebsocketHandler {
+    // Check if the path matches a service id, and close the connection
+    // if not.
+    fn on_request(&mut self, req: &Request) -> Result<Response> {
+        println!("on_request {}", req.resource());
+        let service_id = &req.resource()[1..];
+        println!(" service id is {}", service_id);
+
+        // Hardcoded endpoint where clients can listen to general notifications,
+        // like device joining and departing.
+        if service_id == "services" {
+            let res = try!(Response::from_request(req));
+            return Ok(res);
+        }
+
+        // Look for a service.
+        match self.context.lock().unwrap().get_service(service_id) {
+            None => Err(Error::new(ErrorKind::Internal, "No such service")),
+            Some(_) => {
+                let res = try!(Response::from_request(req));
+                Ok(res)
+            }
+        }
+    }
+
+    fn on_open(&mut self, shake: Handshake) -> Result<()> {
+        println!("on_open {}", shake.request.resource());
+        Ok(())
+    }
 
     fn on_message(&mut self, msg: Message) -> Result<()> {
         // Echo the message back
@@ -35,19 +64,44 @@ impl Handler for WebsocketHandler {
     }
 }
 
+struct WebsocketFactory {
+    context: SharedContext
+}
+
+impl WebsocketFactory {
+    fn new(context: SharedContext) -> WebsocketFactory {
+        WebsocketFactory {
+            context: context
+        }
+    }
+}
+
+impl Factory for WebsocketFactory {
+    type Handler = WebsocketHandler;
+
+    fn connection_made(&mut self, sender: Sender) -> Self::Handler {
+        let ctx = self.context.clone();
+        WebsocketHandler {
+            out: sender,
+            context: ctx
+        }
+    }
+}
+
 impl WebsocketServer {
     pub fn new(context: SharedContext) -> WebsocketServer {
         WebsocketServer { context: context }
     }
 
     pub fn start(&self) {
-        let addrs: Vec<_> = self.context.lock().unwrap().ws_as_addrs().unwrap().collect();
+        let addrs: Vec<_> =
+            self.context.lock().unwrap().ws_as_addrs().unwrap().collect();
 
-        thread::Builder::new().name("WebsocketServer".to_string())
+        let context = self.context.clone();
+        thread::Builder::new().name("WebsocketServer".to_owned())
                               .spawn(move || {
-            listen(addrs[0], |out| {
-                WebsocketHandler { out: out }
-            }).unwrap()
+            let factory = WebsocketFactory::new(context);
+            Builder::new().build(factory).unwrap().listen(addrs[0]).unwrap();
         }).unwrap();
     }
 }
